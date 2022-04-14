f028207fe58d3b3feefe94cc7ba3b96663eba980
Dmitry <BarilkoDA@gmail.com>
2022-04-14 23:28:30 +0300

    The first commit

---
+++ the-first-project/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/* let number = 5;
+const leftBorderWigth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+
+obj.a = 30;
+
+console.log(obj); */
+
+const answers = [];
+
+answers[0] = prompt('Ваше имя', '');
+answers[1] = prompt('Фамилия', '');
+answers[2] = prompt('Сколько лет', '');
+
+document.write(answers);
