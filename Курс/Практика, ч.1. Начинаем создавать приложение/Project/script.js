c822a1f61122b1f325c36eb91d19498a5a1ec080
Dmitry <BarilkoDA@gmail.com>
2022-04-17 23:23:39 +0300

    Solution of the task 2 lesson 15

--- Курс/Практика, ч.1. Начинаем создавать приложение/Project/script.js
+++ Курс/Практика, ч.1. Начинаем создавать приложение/Project/script.js
@@ -1,5 +1,7 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", ""),
-    personalMovieDB = {
+'use strict';
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+      personalMovieDB = {
         count: numberOfFilms, // - сюда передается ответ на первый вопрос
         movies:{}, // - в это свойство поместить пустой объект
         actors:{}, // - тоже поместить пустой объект
@@ -7,32 +9,69 @@ let numberOfFilms = prompt("Сколько фильмов вы уже посмо
         privat:false // - в это свойство поместить boolean(логическое) значение false
     };
 
-let answer1 = prompt("Один из последних просмотренных фильмов?", ""), 
-    answer2 = prompt("На сколько оцените его?", ""),
-    answer3 = prompt("Один из последних просмотренных фильмов?", ""), 
-    answer4 = prompt("На сколько оцените его?", "");
+let answer1,
+    answer2,
+    i = 0;
+// Цикл while
+/* 
+while (i < 2) {
+    while (true) {
+    answer1 = prompt("Один из последних просмотренных фильмов?", "");
+        if (answer1 == "" || answer1 == null || answer1.length > 50) {
+            continue;
+        }
+        else {
+            answer2 = prompt("На сколько оцените его?", "");
+            personalMovieDB.movies[answer1] = answer2;
+            break;
+        }
+    }
+    i++;
+} */
 
-    /* personalMovieDB["movies"] = `${answer1}: ${answer2}, 
-                                 ${answer3}: ${answer4}`; */
+// Цикл do while
 
-/* Несоответствия в решении
+/* do {
+    while (true) {
+        answer1 = prompt("Один из последних просмотренных фильмов?", "");
+            if (answer1 == "" || answer1 == null || answer1.length > 50) {
+                continue;
+            }
+            else {
+                answer2 = prompt("На сколько оцените его?", "");
+                personalMovieDB.movies[answer1] = answer2;
+                break;
+            }
+        }
+        i++;
+}
+while (i < 2); */
 
-1. Отсутствует 'use strict';
-2. Вместо const использовал let
-    Если мы не знаем будет ли изменяться переменная - используем const
-* Если мы предполагаем, что значение будет числом можно поствить унарный +.
-4. Не правильное присвоение данных в объект
-    Привильное
+// Цикл for
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d; 
-    
-Некоторые браузеры не верно работают со значениями через точку. 
-При указании значения через [] - данных проблем нет.
-    */
+for (let i = 0; i < 2; i++)
+{
+    while (true) {
+        answer1 = prompt("Один из последних просмотренных фильмов?", "");
+        if (answer1 == "" || answer1 == null || answer1.length > 50) {
+            continue;
+        }
+        else {
+            answer2 = prompt("На сколько оцените его?", "");
+            personalMovieDB.movies[answer1] = answer2;
+            break;
+        }
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-    
+if (numberOfFilms < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}    
 
 console.log(personalMovieDB);
