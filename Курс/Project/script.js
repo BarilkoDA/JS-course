07a7a2ffc6d8c8ec9c8249e9e0981d3de2217a56
Dmitry <BarilkoDA@gmail.com>
2022-04-24 00:14:17 +0300

    my solution of 18 lesson

---
+++ Курс/Project/script.js
@@ -0,0 +1,76 @@
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
+
+const personalMovieDB = {
+        count: numberOfFilms, // - сюда передается ответ на первый вопрос
+        movies:{}, // - в это свойство поместить пустой объект
+        actors:{}, // - тоже поместить пустой объект
+        genres:[], // - сюда поместить пустой массив
+        privat:false // - в это свойство поместить boolean(логическое) значение false
+    };
+
+
+
+function rememberMyFilms() {
+
+    for (let i = 0; i < 2; i++)
+    {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt("На сколько оцените его?", "");
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersanalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }  
+}
+
+detectPersanalLevel();
+
+function showMyDB(privat) {
+    if(!privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+let genres = [];
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        let question = prompt(`Ваш любимый жанр под номером ${1 + i}`, "");
+        genres[i] = question;
+    }
+}
+
+writeYourGenres();
+console.log(genres);
